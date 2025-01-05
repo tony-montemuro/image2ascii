d49b8c7965a56bbe517ddbca7f8b9cbe7139e68d
tony-montemuro <tonyamontemuro@gmail.com>
2025-01-05 16:56:17 -0600

    added user width + height controls

--- app.go
+++ app.go
@@ -12,6 +12,11 @@ import (
 	"github.com/tony-montemuro/image2ascii/fileio"
 )
 
+const (
+	CHAR_WIDTH  = 2
+	CHAR_HEIGHT = 4
+)
+
 func getMinBrightness() float64 {
 	if len(os.Args) < 3 {
 		return 50.0
@@ -22,6 +27,23 @@ func getMinBrightness() float64 {
 		log.Fatal(err)
 	}
 	return val
+}
+
+func getWidthAndHeight(bounds image.Rectangle) (int, int) {
+	if len(os.Args) < 5 {
+		return int(math.Ceil(float64(bounds.Max.X) / CHAR_WIDTH)), int(math.Ceil(float64(bounds.Max.Y) / CHAR_HEIGHT))
+	}
+
+	width, err := strconv.ParseInt(os.Args[3], 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	height, err := strconv.ParseInt(os.Args[4], 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return int(width), int(height)
 }
 
 func getLinearizedChannel(colorChannel uint8) float64 {
@@ -46,24 +68,6 @@ func getPercievedBrightness(luminance float64) float64 {
 	}
 }
 
-func getPixelBrightness(pixel color.Color) float64 {
-	r, g, b, _ := pixel.RGBA()
-
-	// convert to 8-bit value
-	red := uint8(r >> 8)
-	green := uint8(g >> 8)
-	blue := uint8(b >> 8)
-
-	// liearize red, green, blue
-	lr, lg, lb := getLinearizedChannel(red), getLinearizedChannel(green), getLinearizedChannel(blue)
-
-	// calculate luminance
-	luminance := getLuminance(lr, lg, lb)
-
-	// return percieved brightness
-	return getPercievedBrightness(luminance)
-}
-
 func getPixelNumber(x int, y int) int {
 	if y <= 2 {
 		return 3*x + y
@@ -72,21 +76,54 @@ func getPixelNumber(x int, y int) int {
 }
 
 func generateAscii(img image.Image) []string {
-	const CHAR_WIDTH = 2
-	const CHAR_HEIGHT = 4
+	ascii := []string{}
 	bounds := img.Bounds()
 	minBrightness := getMinBrightness()
+	userWidth, userHeight := getWidthAndHeight(bounds)
+	pixelWidth, pixelHeight := CHAR_WIDTH*userWidth, CHAR_HEIGHT*userHeight
+	originalWidth, originalHeight := bounds.Max.X-bounds.Min.X, bounds.Max.Y-bounds.Min.Y
+	scaleX, scaleY := float64(pixelWidth)/float64(originalWidth), float64(pixelHeight)/float64(originalHeight)
 
-	pixelsToAscii := func(startX int, startY int) rune {
+	pixelsToAscii := func(baseX, baseY int) rune {
+		transformedX, transformedY := baseX*CHAR_WIDTH, baseY*CHAR_HEIGHT
 		var offset uint8 = 0
+
+		getPixelBrightness := func(pixel color.Color) float64 {
+			r, g, b, _ := pixel.RGBA()
+
+			// convert to 8-bit value
+			red := uint8(r >> 8)
+			green := uint8(g >> 8)
+			blue := uint8(b >> 8)
+
+			// linearize red, green, blue
+			lr, lg, lb := getLinearizedChannel(red), getLinearizedChannel(green), getLinearizedChannel(blue)
+
+			// calculate luminance
+			luminance := getLuminance(lr, lg, lb)
+
+			// return percieved brightness
+			return getPercievedBrightness(luminance)
+		}
+
 		for dy := 0; dy < int(CHAR_HEIGHT); dy++ {
 			for dx := 0; dx < int(CHAR_WIDTH); dx++ {
-				x, y := startX+dx, startY+dy
-				if x <= bounds.Max.X && y <= bounds.Max.Y {
-					brightness := getPixelBrightness(img.At(x, y))
-					if brightness > minBrightness {
-						offset |= (1 << getPixelNumber(dx, dy))
-					}
+				// map current pixel to original "pixel"
+				x, y := transformedX+dx, transformedY+dy
+				originalX, originalY := float64(x)/float64(scaleX), float64(y)/float64(scaleY)
+				x1, x2 := int(math.Floor(originalX)), int(math.Ceil(originalX))
+				y1, y2 := int(math.Floor(originalY)), int(math.Ceil(originalY))
+
+				// take the average brightness of all 4 original pixels
+				b1 := getPixelBrightness(img.At(x1, y1))
+				b2 := getPixelBrightness(img.At(x1, y2))
+				b3 := getPixelBrightness(img.At(x2, y1))
+				b4 := getPixelBrightness(img.At(x2, y2))
+				brightness := (b1 + b2 + b3 + b4) / 4
+
+				// if brightness exceeds min brightness, render a pixel (update offset)
+				if brightness >= minBrightness {
+					offset |= (1 << getPixelNumber(dx, dy))
 				}
 			}
 		}
@@ -94,13 +131,15 @@ func generateAscii(img image.Image) []string {
 		return rune(0x2800 + int(offset))
 	}
 
-	ascii := []string{}
-	for y := bounds.Min.Y; y < bounds.Max.Y; y += int(CHAR_HEIGHT) {
+	for y := 0; y < userHeight; y++ {
 		var builder strings.Builder
-		for x := bounds.Min.X; x < bounds.Max.X; x += int(CHAR_WIDTH) {
+		for x := 0; x < userWidth; x++ {
 			builder.WriteRune(pixelsToAscii(x, y))
 		}
-		builder.WriteRune('\n')
+
+		if y < userHeight-1 {
+			builder.WriteRune('\n')
+		}
 		ascii = append(ascii, builder.String())
 	}
 
