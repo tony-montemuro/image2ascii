d34c32f587a1190807a95c802811dc9102c0ce83
tony-montemuro <tonyamontemuro@gmail.com>
2025-01-06 20:39:46 -0600

    converted program to json api

--- app.go
+++ app.go
@@ -1,60 +1,87 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"image"
 	"image/color"
-	"log"
+	_ "image/jpeg"
+	_ "image/png"
 	"math"
-	"os"
+	"net/http"
 	"strconv"
 	"strings"
 
-	"github.com/tony-montemuro/image2ascii/fileio"
+	"github.com/gin-gonic/gin"
 )
 
 const (
-	CHAR_WIDTH  = 2
-	CHAR_HEIGHT = 4
+	CHAR_WIDTH     = 2
+	CHAR_HEIGHT    = 4
+	MAX_BRIGHTNESS = 100.0
 )
 
 type BrightnessComparisonFunc func(a, target float64) bool
 
-func getMinBrightness() float64 {
-	if len(os.Args) < 3 {
-		return 50.0
+func getMinBrightness(c *gin.Context) (float64, error) {
+	brightnessVal, ok := c.GetQuery("brightness")
+
+	if ok {
+		brightness, err := strconv.ParseFloat(brightnessVal, 64)
+		if err != nil {
+			return -1.0, fmt.Errorf("invalid brightness: must be a number")
+		}
+		return MAX_BRIGHTNESS - brightness, nil
 	}
 
-	val, err := strconv.ParseFloat(os.Args[2], 64)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return val
+	return 50.0, nil
 }
 
-func getWidthAndHeight(bounds image.Rectangle) (int, int) {
-	if len(os.Args) < 5 {
-		return int(math.Ceil(float64(bounds.Max.X) / CHAR_WIDTH)), int(math.Ceil(float64(bounds.Max.Y) / CHAR_HEIGHT))
+func getWidthAndHeight(bounds image.Rectangle, c *gin.Context) (int, int, error) {
+	widthVal, okWidth := c.GetQuery("width")
+	heightVal, okHeight := c.GetQuery("height")
+	errs := []string{}
+
+	width, height := 0, 0
+	if !okWidth {
+		width = int(math.Ceil(float64(bounds.Max.X) / CHAR_WIDTH))
+	} else {
+		w, err := strconv.ParseInt(widthVal, 10, 32)
+		if err != nil {
+			errs = append(errs, "invalid width: must be a number")
+		}
+		width = int(w)
 	}
 
-	width, err := strconv.ParseInt(os.Args[3], 10, 64)
-	if err != nil {
-		log.Fatal(err)
-	}
-	height, err := strconv.ParseInt(os.Args[4], 10, 64)
-	if err != nil {
-		log.Fatal(err)
+	if !okHeight {
+		height = int(math.Ceil(float64(bounds.Max.Y) / CHAR_HEIGHT))
+	} else {
+		h, err := strconv.ParseInt(heightVal, 10, 32)
+		if err != nil {
+			errs = append(errs, "invalid height: must be a number")
+		}
+		height = int(h)
 	}
 
-	return int(width), int(height)
+	var err error
+	if len(errs) > 0 {
+		err = errors.New(strings.Join(errs, ", "))
+	}
+	return width, height, err
 }
 
-func isLightInverted() bool {
-	if len(os.Args) < 6 {
-		return false
+func isLightInverted(c *gin.Context) (bool, error) {
+	invertedVal, ok := c.GetQuery("is_inverted")
+
+	if ok {
+		isInverted, err := strconv.ParseBool(invertedVal)
+		if err != nil {
+			return false, fmt.Errorf("invalid inverted: must be either 'true' or 'false'")
+		}
+		return isInverted, nil
 	}
 
-	return os.Args[5] == "1"
+	return false, nil
 }
 
 func getLinearizedChannel(colorChannel uint8) float64 {
@@ -93,17 +120,31 @@ func getBrightnessComparisonFunc(isInverted bool) BrightnessComparisonFunc {
 	return func(brightness, minBrightness float64) bool { return brightness >= minBrightness }
 }
 
-func generateAscii(img image.Image) []string {
+func generateAscii(img image.Image, c *gin.Context) ([]string, int, error) {
 	ascii := []string{}
 	bounds := img.Bounds()
-	minBrightness := getMinBrightness()
-	userWidth, userHeight := getWidthAndHeight(bounds)
-	fmt.Println(userWidth, userHeight)
+
+	// get query params
+	minBrightness, err := getMinBrightness(c)
+	if err != nil {
+		return ascii, http.StatusBadRequest, err
+	}
+
+	userWidth, userHeight, err := getWidthAndHeight(bounds, c)
+	if err != nil {
+		return ascii, http.StatusBadRequest, err
+	}
+
+	isInverted, err := isLightInverted(c)
+	if err != nil {
+		return ascii, http.StatusBadRequest, err
+	}
+	isPixelOn := getBrightnessComparisonFunc(isInverted)
+
+	fmt.Println(minBrightness, userWidth, userHeight, isInverted)
 	pixelWidth, pixelHeight := CHAR_WIDTH*userWidth, CHAR_HEIGHT*userHeight
 	originalWidth, originalHeight := bounds.Max.X-bounds.Min.X, bounds.Max.Y-bounds.Min.Y
 	scaleX, scaleY := float64(pixelWidth)/float64(originalWidth), float64(pixelHeight)/float64(originalHeight)
-	isInverted := isLightInverted()
-	isPixelOn := getBrightnessComparisonFunc(isInverted)
 
 	pixelsToAscii := func(baseX, baseY int) rune {
 		transformedX, transformedY := baseX*CHAR_WIDTH, baseY*CHAR_HEIGHT
@@ -157,30 +198,56 @@ func generateAscii(img image.Image) []string {
 		for x := 0; x < userWidth; x++ {
 			builder.WriteRune(pixelsToAscii(x, y))
 		}
-
-		if y < userHeight-1 {
-			builder.WriteRune('\n')
-		}
 		ascii = append(ascii, builder.String())
 	}
 
-	return ascii
+	return ascii, http.StatusOK, nil
 }
 
-func main() {
-	image, err := fileio.ReadImageFromFile()
-	if err != nil {
-		log.Fatal(err)
-	}
+// func main() {
+// 	image, err := fileio.ReadImageFromFile()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	ascii := generateAscii(image)
-	file, err := os.Create("output.txt")
+// 	ascii := generateAscii(image)
+// 	file, err := os.Create("output.txt")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer file.Close()
+
+// 	for _, row := range ascii {
+// 		file.WriteString(row)
+// 	}
+// }
+
+func getAscii(c *gin.Context) {
+	file, _, err := c.Request.FormFile("image")
 	if err != nil {
-		log.Fatal(err)
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "No image provided"})
+		return
 	}
 	defer file.Close()
 
-	for _, row := range ascii {
-		file.WriteString(row)
+	image, imageType, err := image.Decode(file)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Bad image format: %s", imageType)})
+		return
 	}
+
+	ascii, code, err := generateAscii(image, c)
+	if code != http.StatusOK || err != nil {
+		fmt.Println(code, err)
+		c.IndentedJSON(code, gin.H{"error": err.Error()})
+	} else {
+		c.IndentedJSON(http.StatusOK, ascii)
+	}
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/", getAscii)
+
+	router.Run("localhost:8080")
 }
