11a14e8af52cda1e2553006f2884309e45034be0
tony-montemuro <tonyamontemuro@gmail.com>
2025-01-21 07:58:41 -0600

    readded pixel inversion

--- app.go
+++ app.go
@@ -256,13 +256,6 @@ func getPixelNumber(x int, y int) int {
 	return 2*y + x
 }
 
-func getBrightnessComparisonFunc(isInverted bool) BrightnessComparisonFunc {
-	if isInverted {
-		return func(brightness, minBrightness float64) bool { return brightness <= minBrightness }
-	}
-	return func(brightness, minBrightness float64) bool { return brightness >= minBrightness }
-}
-
 func getFormData(c *gin.Context) (FormData, error) {
 	var form FormData
 
@@ -362,6 +355,24 @@ func pixelsToAscii(point Point, form FormData, grayscaleMatrix [][]float64, enco
 	return rune(0x2800 + int(offset))
 }
 
+func invertBrail(r rune) rune {
+	return r ^ 0xFF
+}
+
+func invertAscii(ascii []string) []string {
+	invertedAscii := make([]string, len(ascii))
+
+	for y, row := range ascii {
+		var builder strings.Builder
+		for _, r := range row {
+			builder.WriteRune(invertBrail(r))
+		}
+		invertedAscii[y] = builder.String()
+	}
+
+	return invertedAscii
+}
+
 func generateAscii(img image.Image, form FormData, encodingSettings EncodingSettings) ([]string, int, error) {
 	ascii := []string{}
 	grayscaleMatrix := getGrayscaleMatrix(img, CHAR_WIDTH**form.Width, CHAR_HEIGHT**form.Height)
@@ -374,181 +385,12 @@ func generateAscii(img image.Image, form FormData, encodingSettings EncodingSett
 		ascii = append(ascii, builder.String())
 	}
 
+	if form.IsInvert.Bool() {
+		ascii = invertAscii(ascii)
+	}
+
 	return ascii, http.StatusOK, nil
 }
-
-// func generateAscii(img image.Image, c *gin.Context) ([]string, int, error) {
-// 	ascii := []string{}
-// 	bounds := img.Bounds()
-
-// 	// get form data
-// 	var form FormData
-// 	if err := c.ShouldBind(&form); err != nil {
-// 		return ascii, http.StatusBadRequest, err
-// 	}
-
-// 	minBrightness, err := getMinBrightness(form)
-// 	if err != nil {
-// 		return ascii, http.StatusBadRequest, err
-// 	}
-
-// 	userWidth, userHeight, err := getWidthAndHeight(bounds, form)
-// 	if err != nil {
-// 		return ascii, http.StatusBadRequest, err
-// 	}
-
-// 	style, err := getStyle(form)
-// 	if err != nil {
-// 		return ascii, http.StatusBadRequest, err
-// 	}
-
-// 	isInverted := form.IsInvert.Bool()
-// 	isPixelOn := getBrightnessComparisonFunc(isInverted)
-
-// 	totalWidth, totalHeight := CHAR_WIDTH*userWidth, CHAR_HEIGHT*userHeight
-// 	originalWidth, originalHeight := bounds.Max.X-bounds.Min.X, bounds.Max.Y-bounds.Min.Y
-// 	scaleX, scaleY := float64(totalWidth)/float64(originalWidth), float64(totalHeight)/float64(originalHeight)
-
-// 	pixelsToAscii := func(baseX, baseY int) rune {
-// 		transformedX, transformedY := baseX*CHAR_WIDTH, baseY*CHAR_HEIGHT
-// 		var offset uint8 = 0
-
-// 		getPixelBrightness := func(pixel color.Color) float64 {
-// 			luminance := getPixelLuminance(pixel)
-
-// 			// return percieved brightness
-// 			return getPercievedBrightness(luminance)
-// 		}
-
-// 		for dy := 0; dy < int(CHAR_HEIGHT); dy++ {
-// 			for dx := 0; dx < int(CHAR_WIDTH); dx++ {
-// 				// map current pixel to original "pixel"
-// 				x, y := transformedX+dx, transformedY+dy
-// 				originalX, originalY := float64(x)/float64(scaleX), float64(y)/float64(scaleY)
-// 				x1, x2 := int(math.Floor(originalX)), int(math.Ceil(originalX))
-// 				y1, y2 := int(math.Floor(originalY)), int(math.Ceil(originalY))
-
-// 				// take the average brightness of all 4 original pixels
-// 				b1 := getPixelBrightness(img.At(x1, y1))
-// 				b2 := getPixelBrightness(img.At(x1, y2))
-// 				b3 := getPixelBrightness(img.At(x2, y1))
-// 				b4 := getPixelBrightness(img.At(x2, y2))
-// 				brightness := (b1 + b2 + b3 + b4) / 4
-
-// 				// if brightness exceeds min brightness, render a pixel (update offset)
-// 				if isPixelOn(brightness, minBrightness) {
-// 					offset |= (1 << getPixelNumber(dx, dy))
-// 				}
-// 			}
-// 		}
-
-// 		return rune(0x2800 + int(offset))
-// 	}
-
-// 	// setup oldPixels storage, used by floyd-steinberg dithering
-// 	oldPixels := make([][]float64, totalHeight)
-// 	for y := range oldPixels {
-// 		oldPixels[y] = make([]float64, totalWidth)
-// 		for x := range oldPixels[y] {
-// 			oldPixels[y][x] = -1.0
-// 		}
-// 	}
-// 	oldPixels[0][0] = getPixelLuminance(img.At(0, 0))
-
-// 	pixelsToAscii2 := func(baseX, baseY int) rune {
-// 		getOriginalCoords := func(x, y int) (int, int) {
-// 			originalX, originalY := float64(x)/float64(scaleX), float64(y)/float64(scaleY)
-// 			return int(math.Round(originalX)), int(math.Round(originalY))
-// 		}
-
-// 		transformedX, transformedY := baseX*CHAR_WIDTH, baseY*CHAR_HEIGHT
-// 		var offset uint8 = 0
-
-// 		for dy := 0; dy < int(CHAR_HEIGHT); dy++ {
-// 			for dx := 0; dx < int(CHAR_WIDTH); dx++ {
-// 				x, y := transformedX+dx, transformedY+dy
-// 				luminance := oldPixels[y][x]
-// 				brightness := getPercievedBrightness(luminance)
-// 				isPixelEnabled := isPixelOn(brightness, minBrightness)
-// 				quantError := luminance
-// 				// if isInverted {
-// 				// 	if brightness <= minBrightness {
-// 				// 		offset |= (1 << getPixelNumber(dx, dy))
-// 				// 		quantError = luminance - 1.0
-// 				// 	}
-// 				// } else {
-// 				// 	if brightness >= minBrightness {
-// 				// 		offset |= (1 << getPixelNumber(dx, dy))
-// 				// 		quantError = luminance - 1.0
-// 				// 	}
-// 				// }
-// 				if isPixelEnabled {
-// 					offset |= (1 << getPixelNumber(dx, dy))
-// 					if !isInverted {
-// 						quantError = luminance - 1.0
-// 					}
-// 				} else {
-// 					if isInverted {
-// 						quantError = luminance - 1.0
-// 					}
-// 				}
-
-// 				// dithering
-// 				if x+1 < totalWidth {
-// 					if oldPixels[y][x+1] == -1.0 {
-// 						originalX, originalY := getOriginalCoords(x+1, y)
-// 						oldPixels[y][x+1] = getPixelLuminance(img.At(originalX, originalY))
-// 					}
-// 					oldPixels[y][x+1] = oldPixels[y][x+1] + quantError*(7.0/16.0)
-// 				}
-// 				if x-1 >= 0 && y+1 < totalHeight {
-// 					if oldPixels[y+1][x-1] == -1.0 {
-// 						originalX, originalY := getOriginalCoords(x-1, y+1)
-// 						oldPixels[y+1][x-1] = getPixelLuminance(img.At(originalX, originalY))
-// 					}
-// 					oldPixels[y+1][x-1] = oldPixels[y+1][x-1] + quantError*(3.0/16.0)
-// 				}
-// 				if y+1 < totalHeight {
-// 					if oldPixels[y+1][x] == -1.0 {
-// 						originalX, originalY := getOriginalCoords(x, y+1)
-// 						oldPixels[y+1][x] = getPixelLuminance(img.At(originalX, originalY))
-// 					}
-// 					oldPixels[y+1][x] = oldPixels[y+1][x] + quantError*(5.0/16.0)
-// 				}
-// 				if y+1 < totalHeight && x+1 < totalWidth {
-// 					if oldPixels[y+1][x+1] == -1.0 {
-// 						originalX, originalY := getOriginalCoords(x+1, y+1)
-// 						oldPixels[y+1][x+1] = getPixelLuminance(img.At(originalX, originalY))
-// 					}
-// 					oldPixels[y+1][x+1] = oldPixels[y+1][x+1] + quantError*(1.0/16.0)
-// 				}
-// 			}
-// 		}
-
-// 		return rune(0x2800 + int(offset))
-// 	}
-
-// 	switch style {
-// 	case STYLE_BRIGHTNESS:
-// 		for y := 0; y < userHeight; y++ {
-// 			var builder strings.Builder
-// 			for x := 0; x < userWidth; x++ {
-// 				builder.WriteRune(pixelsToAscii(x, y))
-// 			}
-// 			ascii = append(ascii, builder.String())
-// 		}
-// 	case STYLE_NORMAL:
-// 		for y := 0; y < userHeight; y++ {
-// 			var builder strings.Builder
-// 			for x := 0; x < userWidth; x++ {
-// 				builder.WriteRune(pixelsToAscii2(x, y))
-// 			}
-// 			ascii = append(ascii, builder.String())
-// 		}
-// 	}
-
-// 	return ascii, http.StatusOK, nil
-// }
 
 func getAscii(c *gin.Context) {
 	// attempt to open image, and validate it
